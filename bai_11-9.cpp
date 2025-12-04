e20f92fee378f4caf250b403c875c103fb7c6c26
iamnotproatcoding <sang270310@gmail.com>
2025-12-04 19:38:10 +0700

    initial commit

---
+++ bai_11-9.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << i << " ";
+    }
+    return 0;
+}
