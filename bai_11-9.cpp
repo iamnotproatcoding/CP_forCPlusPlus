68e34b9e050518a3b6bde1e4158b0036f33dfcc7
iamnotproatcoding <sang270310@gmail.com>
2025-12-09 17:30:56 +0700

    Finished 11.9

--- bai_11-9.cpp
+++ bai_11-9.cpp
@@ -3,12 +3,27 @@
 using namespace std;
 
 signed main() {
+    ios_base::sync_with_stdio(); cin.tie(0);
+
     int n;
     cin >> n;
 
+    vector<int> a(n);
+    int max = 0;
     for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
+        cin >> a[i];
+        if (a[i] > max) {
+            max = a[i];
+        }
     }
-    return 0;
+
+    sort(a.begin(), a.end());
+    for (int i = 1; i < n; i++) {
+        if (a[i] - 1 > a[i - 1]) {
+            cout << a[i - 1] + 1;
+            return 0;
+        }
+    }
+
+    cout << max + 1;
 }
